b598c229664796b559ded3e74a6cdf1741d51100
Michael Comella <michael.l.comella@gmail.com>
2013-11-15 11:52:13 -0800

    Fix testNewAboutHomeSwipes for tablets.

--- testNewAboutHomeSwipes.java
+++ testNewAboutHomeSwipes.java
@@ -9,6 +9,9 @@ import org.mozilla.gecko.tests.helpers.*;
 /* Test correct state for URL bar after loading pages.
  */
 public class testNewAboutHomeSwipes extends UITest {
+    // TODO: Define this test dynamically by creating dynamic representations of the Page
+    // enum for both phone and tablet, then swiping through the pages. This will also
+    // benefit having a HomePager with custom pages.
     public void testNewAboutHomeSwipes() {
         GeckoHelper.blockForReady();
 
@@ -21,24 +24,46 @@ public class testNewAboutHomeSwipes extends UITest {
         mAboutHome.swipeToNext();
         mAboutHome.assertCurrentPage(Page.READING_LIST);
 
-        // Edge case.
-        mAboutHome.swipeToNext();
-        mAboutHome.assertCurrentPage(Page.READING_LIST);
+        if (!DeviceHelper.isTablet()) {
+            // Edge case.
+            mAboutHome.swipeToNext();
+            mAboutHome.assertCurrentPage(Page.READING_LIST);
 
-        mAboutHome.swipeToPrevious();
-        mAboutHome.assertCurrentPage(Page.BOOKMARKS);
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.BOOKMARKS);
 
-        mAboutHome.swipeToPrevious();
-        mAboutHome.assertCurrentPage(Page.TOP_SITES);
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.TOP_SITES);
 
-        mAboutHome.swipeToPrevious();
-        mAboutHome.assertCurrentPage(Page.HISTORY);
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.HISTORY);
 
-        // Edge case.
-        mAboutHome.swipeToPrevious();
-        mAboutHome.assertCurrentPage(Page.HISTORY);
+            // Edge case.
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.HISTORY);
 
-        mAboutHome.swipeToNext();
-        mAboutHome.assertCurrentPage(Page.TOP_SITES);
+            mAboutHome.swipeToNext();
+            mAboutHome.assertCurrentPage(Page.TOP_SITES);
+        } else {
+            mAboutHome.swipeToNext();
+            mAboutHome.assertCurrentPage(Page.HISTORY);
+
+            // Edge case.
+            mAboutHome.swipeToNext();
+            mAboutHome.assertCurrentPage(Page.HISTORY);
+
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.READING_LIST);
+
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.BOOKMARKS);
+
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.TOP_SITES);
+
+            // Edge case.
+            mAboutHome.swipeToPrevious();
+            mAboutHome.assertCurrentPage(Page.TOP_SITES);
+        }
     }
 }
